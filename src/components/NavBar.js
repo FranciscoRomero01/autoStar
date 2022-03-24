4f2b45c731243aaf1ee82b59665b92c3f1a9ca66
F.E.R_Studios <romerofranciscoelias01@gmail.com>
2022-03-24 13:39:00 -0300

    se creo un NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+
+function NavBar(){
+    return(
+        <nav>
+            <div className="nav-wrapper black">
+                <a href="#" className="brand-logo center white-text">AutoStar</a>
+                <ul id="nav-mobile" className="left hide-on-med-and-down">
+                    <li><a href="#" className="white-text">Inicio</a></li>
+                    <li><a href="#" className="white-text">Servicios</a></li>
+                    <li><a href="#" className="white-text">Novedades</a></li>
+                    <li><a href="#" className="white-text">Contacto</a></li>
+                </ul>
+                <ul className="right">
+                    <li><a href="#" className="white-text">Iniciar Sesion</a></li>
+                    <li><a href="#" className="white-text">Registrarse</a></li>
+                </ul>
+            </div>
+      </nav>
+    )
+}
+
+export default NavBar;
+
